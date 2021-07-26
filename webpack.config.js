faf3615fe6948891faabb0e0a5720a01f000e5bd
Carlos Urquiza <webquiza@gmail.com>
2021-07-25 19:22:20 -0700

    manage images with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,24 @@ module.exports = {
   ],
   module: {
     rules: [
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
      {
         test: /\.css$/,
         use: [
